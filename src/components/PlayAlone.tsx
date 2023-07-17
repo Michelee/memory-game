b039ab9868dd95189afdbf9fb5b0a91efc1d9290
Michele Ancheta <michele.ancheta@ximedes.com>
2023-07-17 16:12:45 +0200

    update play alone

--- src/components/PlayAlone.tsx
+++ src/components/PlayAlone.tsx
@@ -3,7 +3,9 @@
 import { CardsProps, PlayersProps } from "@/utils/gameTypes";
 import React, { use, useEffect, useState } from "react";
 
+import { Button } from "./Button";
 import CardComponent from "@/components/CardComponent";
+import { useRouter } from "next/navigation";
 
 interface PlayAloneProps {
   gameCards: CardsProps[];
@@ -12,6 +14,8 @@ interface PlayAloneProps {
 const PlayAlone = ({ gameCards }: PlayAloneProps) => {
   const [cards, setCards] = useState<CardsProps[]>(gameCards);
   const [score, setScore] = useState(0);
+
+  const router = useRouter();
 
   useEffect(() => {
     const newCards = [...cards];
@@ -36,10 +40,9 @@ const PlayAlone = ({ gameCards }: PlayAloneProps) => {
             }
           });
         }
+        setCards(newCards);
       }
-      setCards(newCards);
     }, 1000);
-
   }, [cards, score]);
 
   const handleClick = (index: number) => {
@@ -48,29 +51,61 @@ const PlayAlone = ({ gameCards }: PlayAloneProps) => {
     setCards(newCards);
   };
 
+  const handleTryAgain = () => {
+    const newCards = [...cards];
+    newCards.forEach((card) => {
+      card.flipped = false;
+      card.matched = false;
+    });
+    setCards(newCards);
+    setScore(0);
+  };
+
   return (
     <div className="flex flex-col max-w-screen-md mb-8 p-5 my-0 mx-auto">
-      <div className="my-5 flex justify-between">
-        <span className="text-xl">Score</span>
-        <span className="text-xl">{score}</span>
-      </div>
+      {score === 8 ? (
+        <div className="items-center flex flex-col my-10">
+          <div className="text-2xl inline-block mx-auto my-10">You Win!</div>
+          <div className="justify-between flex gap-4">
+            <Button
+              label="Try Again"
+              onClick={handleTryAgain}
+              classes="bg-green-500 px-4"
+            />
 
-      <div
-        className={`grid gap-2 ${
-          cards.length > 16 ? "grid-cols-6" : "grid-cols-4"
-        }`}
-      >
-        {cards.map((props, index) => (
-          <CardComponent
-            key={index}
-            value={props.card}
-            flipped={props.flipped}
-            matched={props.matched}
-            disabled={cards.filter((e) => e.flipped && !e.matched).length === 2}
-            handleClick={() => handleClick(index)}
-          />
-        ))}
-      </div>
+            <Button
+              label="Go Home"
+              onClick={() => router.push("/")}
+              classes="bg-orange-500 px-4"
+            />
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className="my-5 flex justify-between">
+            <span className="text-xl">Score</span>
+            <span className="text-xl">{score}</span>
+          </div>
+          <div
+            className={`grid gap-2 ${
+              cards.length > 16 ? "grid-cols-6" : "grid-cols-4"
+            }`}
+          >
+            {cards.map((props, index) => (
+              <CardComponent
+                key={index}
+                value={props.card}
+                flipped={props.flipped}
+                matched={props.matched}
+                disabled={
+                  cards.filter((e) => e.flipped && !e.matched).length === 2
+                }
+                handleClick={() => handleClick(index)}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
